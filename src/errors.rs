b45ca415a2fe092267ee57d86c1ad15e9b421227
Dylan Anthony <contact@dylananthony.com>
2021-04-03 16:06:15 -0600

    refactor: Make Handler stuff generic and put in own module for easier reuse

---
+++ src/errors.rs
@@ -0,0 +1,25 @@
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub(crate) enum ServerError {
+    #[error("could not serialize JSON")]
+    Disconnect(#[from] serde_json::Error),
+    #[error("error creating response")]
+    Response(#[from] http::Error),
+}
+
+#[derive(Error, Debug)]
+pub(crate) enum ClientError {
+    #[error(transparent)]
+    Query(#[from] async_graphql::ParseRequestError),
+    #[error(transparent)]
+    Json(#[from] serde_json::Error),
+    #[error("POST request must contain a body")]
+    EmptyBody,
+    #[error("Binary body must be encoded with UTF-8")]
+    InvalidBinaryBody(#[from] std::str::Utf8Error),
+    #[error("Only GET and POST methods are allowed")]
+    MethodNotAllowed,
+    #[error("query param is required with GET method")]
+    MissingQuery,
+}
---
+++ src/handler.rs
@@ -0,0 +1,163 @@
+use std::future::Future;
+use std::pin::Pin;
+
+use async_graphql::{ObjectType, Request as GraphQLRequest, Schema, SubscriptionType};
+use aws_lambda_events::encodings::Body;
+use aws_lambda_events::event::apigw::{
+    ApiGatewayProxyRequestContext, ApiGatewayV2httpRequestContext,
+};
+use http::{Method, StatusCode};
+use lamedh_http::request::RequestContext;
+use lamedh_http::{Context, Handler, Request, RequestExt, Response};
+
+use crate::errors::{ClientError, ServerError};
+
+pub(crate) struct GraphQLHandler<Query, Mutation, Subscription>
+where
+    Query: ObjectType + 'static,
+    Mutation: ObjectType + 'static,
+    Subscription: SubscriptionType + 'static,
+{
+    schema: Schema<Query, Mutation, Subscription>,
+}
+
+impl<Query, Mutation, Subscription> GraphQLHandler<Query, Mutation, Subscription>
+where
+    Query: ObjectType + 'static,
+    Mutation: ObjectType + 'static,
+    Subscription: SubscriptionType + 'static,
+{
+    pub fn new(
+        schema: Schema<Query, Mutation, Subscription>,
+    ) -> GraphQLHandler<Query, Mutation, Subscription>
+    where
+        Query: ObjectType + 'static,
+        Mutation: ObjectType + 'static,
+        Subscription: SubscriptionType + 'static,
+    {
+        GraphQLHandler { schema }
+    }
+}
+
+type HandlerError = ServerError;
+type HandlerResponse = Response<String>;
+type HandlerResult = Result<HandlerResponse, HandlerError>;
+
+impl<Query, Mutation, Subscription> Handler for GraphQLHandler<Query, Mutation, Subscription>
+where
+    Query: ObjectType + 'static,
+    Mutation: ObjectType + 'static,
+    Subscription: SubscriptionType + 'static,
+{
+    type Error = ServerError;
+    type Response = HandlerResponse;
+
+    type Fut = Pin<Box<dyn Future<Output = HandlerResult> + 'static>>;
+
+    fn call(&mut self, req: Request, _ctx: Context) -> Self::Fut {
+        let schema = self.schema.clone();
+        let fut = async move { handle_request(schema, req).await };
+        Box::pin(fut)
+    }
+}
+
+async fn handle_request<Query, Mutation, Subscription>(
+    schema: Schema<Query, Mutation, Subscription>,
+    request: Request,
+) -> Result<HandlerResponse, ServerError>
+where
+    Query: ObjectType + 'static,
+    Mutation: ObjectType + 'static,
+    Subscription: SubscriptionType + 'static,
+{
+    let path = request.uri().path();
+    if path == "/" {
+        Response::builder()
+            .header("Content-Type", "text/html")
+            .body(async_graphql::http::graphiql_source(
+                &graphql_endpoint_with_stage(request.request_context()),
+                None,
+            ))
+            .map_err(ServerError::from)
+    } else if path.ends_with("/graphql") {
+        handle_query(schema, request).await
+    } else {
+        error_response(http::StatusCode::NOT_FOUND, "Invalid path".to_string())
+    }
+}
+
+fn graphql_endpoint_with_stage(request_context: RequestContext) -> String {
+    let stage = match request_context {
+        RequestContext::ApiGatewayV1(ApiGatewayProxyRequestContext { stage, .. }) => stage,
+        RequestContext::ApiGatewayV2(ApiGatewayV2httpRequestContext { stage, .. }) => stage,
+        RequestContext::Alb(..) => None,
+    };
+    match stage {
+        Some(stage) => format!("/{}/graphql", stage),
+        None => "/graphql".to_string(),
+    }
+}
+
+async fn handle_query<Query, Mutation, Subscription>(
+    schema: Schema<Query, Mutation, Subscription>,
+    request: Request,
+) -> Result<HandlerResponse, ServerError>
+where
+    Query: ObjectType + 'static,
+    Mutation: ObjectType + 'static,
+    Subscription: SubscriptionType + 'static,
+{
+    let query = if request.method() == Method::POST {
+        graphql_request_from_post(request)
+    } else if request.method() == Method::GET {
+        graphql_request_from_get(request)
+    } else {
+        Err(ClientError::MethodNotAllowed)
+    };
+    let query = match query {
+        Err(e) => {
+            return error_response(StatusCode::BAD_REQUEST, format!("{}", e));
+        }
+        Ok(query) => query,
+    };
+    let response_body =
+        serde_json::to_string(&schema.execute(query).await).map_err(ServerError::from)?;
+    Response::builder()
+        .status(200)
+        .body(response_body)
+        .map_err(|e| e.into())
+}
+
+fn error_response(status: http::StatusCode, body: String) -> Result<Response<String>, ServerError> {
+    Response::builder()
+        .status(status)
+        .body(body)
+        .map_err(ServerError::from)
+}
+
+fn graphql_request_from_post(request: Request) -> Result<GraphQLRequest, ClientError> {
+    match request.into_body() {
+        Body::Empty => Err(ClientError::EmptyBody),
+        Body::Text(text) => {
+            serde_json::from_str::<GraphQLRequest>(&text).map_err(ClientError::from)
+        }
+        Body::Binary(binary) => {
+            serde_json::from_slice::<GraphQLRequest>(&binary).map_err(ClientError::from)
+        }
+    }
+}
+
+fn graphql_request_from_get(request: Request) -> Result<GraphQLRequest, ClientError> {
+    let params = request.query_string_parameters();
+    let query = params.get("query").ok_or(ClientError::MissingQuery)?;
+    let mut request = async_graphql::Request::new(query);
+    if let Some(operation_name) = params.get("operationName") {
+        request = request.operation_name(operation_name)
+    }
+    if let Some(variables) = params.get("variables") {
+        let value = serde_json::from_str(&variables).unwrap_or_default();
+        let variables = async_graphql::Variables::from_json(value);
+        request = request.variables(variables);
+    }
+    Ok(request)
+}
