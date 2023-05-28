a7995f80cc1ae0b4b688aa32dacb1d11da6609f3
Dylan Anthony <dbanty@users.noreply.github.com>
2023-05-28 15:22:31 -0600

    fix: Remove runtime need for regex to shrink bin size & build times

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
+use std::str::FromStr;
+
 use handler::handle_request;
 use lambda_http::{run, Error};
 use lambda_runtime::service_fn;
 use tracing::subscriber::set_global_default;
 use tracing_log::LogTracer;
-use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
+use tracing_subscriber::{filter::Targets, layer::SubscriberExt, Registry};
 
 mod errors;
 mod handler;
@@ -19,7 +21,9 @@ async fn main() -> Result<(), Error> {
 fn setup_tracing() {
     LogTracer::init().expect("Failed to set logger");
 
-    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
+    let log_levels = std::env::var("RUST_LOG").unwrap_or_else(|_| "info".to_string());
+    let env_filter =
+        Targets::from_str(&log_levels).expect("Failed to parse log levels from RUST_LOG env var");
     let subscriber = Registry::default().with(env_filter);
     set_global_default(subscriber).expect("Failed to set subscriber");
 }
